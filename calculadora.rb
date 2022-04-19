f141db00c14fa165e3fbce552e0a52498dcb9fa4
GABRIEL OLIVEIRA <gabriel.oliveira@euax.com.br>
2022-04-19 15:03:19 -0300

    Refatoração para tratamento de divisão por zero, tratamento de erro para inclusão de números além dos disponíveis no menu, refatoração para tratamento de erros para inserção de input não numérico e tratamento de saída para cálculos matemáticos que não sejam de porcentagem e um exclusivo de porcentagem

--- calculadora.rb
+++ calculadora.rb
@@ -3,19 +3,25 @@ class Calculadora
         @value1 = value1
         @value2 = value2
     end
-  
+
     def soma 
         @value1 + @value2
     end
-  
+
     def subtracao
         @value1 - @value2
     end
-  
+
     def divisao
         @value1 / @value2
+        if @value2 != 0
+            @value1 / @value2
+        else
+            puts "ERRO: divisão por zero não permitida. Calculadora desligando."
+            exit
+        end
     end
-  
+
     def multiplicacao
         @value1 * @value2
     end
@@ -24,28 +30,28 @@ class Calculadora
         (@value1 * @value2) / 100
     end
 end
-  
+
 print "Qual operação matemática você deseja realizar? Digite: \n '1' para soma \n '2' para subtração \n '3' para divisão\n '4' para multiplicação \n '5' para porcentagem. Primeiro valor porcentagem e segundo o valor do número. \n Por exemplo: 6% (6 = primeiro valor) de 100 (segundo valor): "
-operacao = gets.chomp.to_i
+operacao = gets.chomp.to_i    
+    if operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4 && operacao != 5
+        puts "ERRO: operação inválida. Calculadora desligando."
+        exit
+    end
 
 print "Insira o primeiro número: "
 value1 = gets.chomp()
 
-if value1 =~ /^-?[0-9]+$/
-    puts "Informação válida."
-else  
-    puts "ERRO: você não forneceu um número. Calculadora desligando."
-    exit(1)   
+unless value1 =~ /^-?[0-9]+$/
+    puts "ERRO: você não forneceu um número válido. Calculadora desligando."
+    exit
 end
 
 print "Insira o segundo número: "
 value2 = gets.chomp()
 
-if value2 =~ /^-?[0-9]+$/
-    puts "Informação válida."
-else  
-    puts "ERRO: você não forneceu um número. Calculadora desligando."
-    exit(1)   
+unless value2 =~ /^-?[0-9]+$/
+    puts "ERRO: você não forneceu um número válido. Calculadora desligando."
+    exit 
 end
 
 case operacao
@@ -59,12 +65,13 @@ case operacao
         operacao = :multiplicacao
     when 5
         operacao = :porcentagem
-    else
-        puts "ERRO: Você não forneceu a operação possível a ser realizada."
-        exit(1)
 end
-  
+
 calculadora = Calculadora.new(value1.to_f, value2.to_f)
+
 resultado = calculadora.public_send(operacao)
-  
-puts "Resultado da #{operacao} é: #{resultado}"
+    if operacao != :porcentagem
+        puts "Resultado da #{operacao} é: #{resultado}."
+    else 
+        puts "#{value1}% de #{value2} é: #{resultado}."
+    end
